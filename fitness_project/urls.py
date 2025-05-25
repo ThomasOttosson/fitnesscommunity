c3ade132a41be6883e58299e9e827dc825491fe7
Thomas Ottosson <tomasottosson1@gmail.com>
2025-05-25 17:41:32 +0200

    testing

--- fitness_project/urls.py
+++ fitness_project/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("home.urls"), name="home-urls"),
-    path('summernote/', include('django_summernote.urls')),
     path("about/", include("about.urls"), name="about-urls"),
+    path('summernote/', include('django_summernote.urls')),
     path("accounts/", include("allauth.urls")),
 ]
